e11940319e14fc33646c08c62c210237282df5c6
ricardotg34 <loiztg34@gmail.com>
2019-03-09 13:19:09 -0600

    DROP MENU

--- calculadora.py
+++ calculadora.py
@@ -1,31 +1,3 @@
 def product(a,b):
     """This function multiply two numbres : a and b"""
     return (a*b)
-
-def menu():
-    print("\n\tCALCULATOR\n")
-    print("1. Addition.")
-    print("2. Subtraction.")
-    print("3. Product")
-    print("4. Quotient")
-    print("5. Pow")
-    a=input("Enter first number:")
-    b=input("Enter second number:")
-    option = input("Choose an option: ")
-    if option=='1':
-        r=sumar(a,b)
-    elif option=='2':
-        r=sub(a,b)
-    elif option=="3":
-        r=product(a,b)
-    elif option=="4":
-        r=dividir(a,b)
-    elif option=="5":
-        r=potencia(a,b)
-    else:
-        print("INEXISTENT OPTION")
-    print("The result is: "+r)
-
-if __name__ == '__main__':
-    while True:
-        menu()
